7599deb624fe2802e92f4b1d242e167a049a0eb2
eejai42 <start@anabstractlevel.com>
2026-05-29 13:24:44 -0500

    Add migration for customers table in Rails
    - Introduced a new migration file to create the `customers` table with fields for `customer_id`, `email_address`, `first_name`, and `last_name`.
    - Set `customer_id` as the primary key to ensure unique identification of customers.
    
    This addition lays the foundation for managing customer data within the application.

---
+++ rulebook-examples/customer-fullname/execution-substrates/rails/db/migrate/1780079066526_customers.rb
@@ -0,0 +1,10 @@
+class CreateCustomers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :customers, primary_key: :customer_id, id: false do |t|
+      t.string :customer_id, primary_key: true
+      t.string :email_address
+      t.string :first_name
+      t.string :last_name
+    end
+  end
+end
