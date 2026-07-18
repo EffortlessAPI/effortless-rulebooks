bc16a0115bd8e2e0bd7b48cba80abbf0f5560376
EJ Alexandra <start@anabstractlevel.com>
2026-07-18 15:47:16 -0500

    Replay historical TSP assertions through temporal validator projection

--- rulebook-examples/effortless-math/domains/traveling-salesman/scripts/validate_rulebook.py
+++ rulebook-examples/effortless-math/domains/traveling-salesman/scripts/validate_rulebook.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from validate_rulebook_v3 import main as validate_v3
+from validate_rulebook_v3_temporal import main as validate_v3
 from validate_rulebook_v5 import validate_repository_state
 from validate_summary_alignment_v5 import validate_summary_alignment
 
