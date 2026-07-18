3c29f13c2f778baeebfdd20b0eef25fc14657b00
EJ Alexandra <start@anabstractlevel.com>
2026-07-17 21:53:36 -0500

    Run TSP semantic and summary validators together

--- rulebook-examples/effortless-math/domains/traveling-salesman/scripts/validate_rulebook.py
+++ rulebook-examples/effortless-math/domains/traveling-salesman/scripts/validate_rulebook.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-from validate_rulebook_v3 import main
+from validate_rulebook_v3 import main as validate_rulebook
+from validate_summary_alignment import main as validate_summary
 
-if __name__ == '__main__':
-    main()
+
+if __name__ == "__main__":
+    validate_rulebook()
+    validate_summary()
